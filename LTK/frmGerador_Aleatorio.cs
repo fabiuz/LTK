ecd3f6593d5e26d5fde0f144dac2aedf4bf603e2
Fábio Moura de Oliveira <fbsharp@hotmail.com>
2016-07-03 16:37:25 -0300

    Cria a estrutura 'JogoInfo' para guardar todas as informações relativas de um jogo.

--- LTK/frmGerador_Aleatorio.cs
+++ LTK/frmGerador_Aleatorio.cs
@@ -10,58 +10,83 @@ using System.Windows.Forms;
 
 namespace LTK
 {
+    /// <summary>
+    ///     Guarda todas as informações referente a úm único jogo.
+    /// </summary>
+    internal struct JogoInfo
+    {
+        public JogoInfo(string jogoTipo, int menorBola, int maiorBola, int quantidadeBola, int[] bolasApostadas)
+        {
+            this.jogoTipo = jogoTipo;
+            this.menorBola = menorBola;
+            this.maiorBola = maiorBola;
+            this.quantidadeBola = quantidadeBola;
+            this.bolasApostadas = bolasApostadas;
+        }
+
+        public string jogoTipo { get; set; }
+        public int menorBola { get; set; }
+        public int maiorBola { get; set; }
+        public int quantidadeBola { get; set; }
+        public int[] bolasApostadas { get; set; }
+
+    }
     public partial class frmGerador_Aleatorio : Form
     {
+        private JogoInfo[] jogoInfo;
+
+        private void InicializarJogoInfo()
+        {
+            jogoInfo = new JogoInfo[]
+            {
+                new JogoInfo("QUINA", 1, 80, 80, new int [] {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }),
+                new JogoInfo("MEGASENA", 1, 60, 60, new int [] {6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }),
+                new JogoInfo("LOTOFACIL", 1, 25, 25, new int [] {15, 16, 17, 18}),
+                new JogoInfo("LOTOMANIA", 0, 99, 100, new int[] {50 }),
+                new JogoInfo("DUPLASENA", 1, 50, 50, new int[] {6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }),
+                new JogoInfo("INTRALOT_MINAS_5", 1, 34, 34, new int[] {5 }),
+                new JogoInfo("INTRALOT_LOTO_MINAS", 1, 38, 38, new int[] {6 }),
+                new JogoInfo("INTRALOT_KENO_MINAS", 1, 80, 80, new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1 })
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public frmGerador_Aleatorio()
         {
             InitializeComponent();
+
+            // Instancia e cria a informação de cada jogo.
+            InicializarJogoInfo();
+
 
             // Vamos preencher a lista conforme o tipo do jogo.
             jogo_aposta = new Dictionary<string, string[]>()
             {
                 {
-                    "QUINA", new string[] {"com 5 números.", "com 6 números.", "com 7 números."}
+                    "QUINA", new string[] {"5", "6", "7"}
                 },
                 {
-                    "MEGASENA",
-                    new string[]
-                    {
-                        "com 6 números.",  "com 7 números.",  "com 8 números.", "com 9 números.",
-                        "com 10 números.", "com 11 números.", "com 12 números.", "com 13 números.",
-                        "com 14 números.", "com 15 números."
-                    }
+                    "MEGASENA", new string[] {"6", "7", "8", "9", "10", "11", "12", "13", "14", "15"}
                 },
                 {
-                    "LOTOFACIL",
-                    new string[]
-                    {
-                        "com 15 números.", "com 16 números.", "com 17 números.", "com 18 números."
-                    }
+                    "LOTOFACIL", new string[] {"15", "16", "17", "18" }
                 },
                 {
-                    "LOTOMANIA", new string[] {"com 50 números." }
+                    "LOTOMANIA", new string[] {"50" }
                 },
                 {
-                    "DUPLASENA",
-                    new string[]
-                    {
-                        "com 6 números.",  "com 7 números.",  "com 8 números.", "com 9 números.",
-                        "com 10 números.", "com 11 números.", "com 12 números.", "com 13 números.",
-                        "com 14 números.", "com 15 números."
-                    }
+                    "DUPLASENA",new string[] {"6", "7", "8", "9", "10", "11", "12", "13", "14", "15"}
                 },
                 {
-                    "INTRALOT_MINAS_5", new string[] {"com 5 números." }
+                    "INTRALOT_MINAS_5", new string[] {"5" }
                 },
                 {
-                    "INTRALOT_LOTO_MINAS", new string[] {"com 6 números." }
+                    "INTRALOT_LOTO_MINAS", new string[] {"6" }
                 },
                 {
-                    "INTRALOT_KENO_MINAS",
-                    new string[] {
-                        "com 1 número.", "com 2 números.", "com 3 números.", "com 4 números.", "com 5 números.",
-                        "com 6 números.", "com 7 números.", "com 8 números.", "com 9 números.", "com 10 números."
-                    }
+                    "INTRALOT_KENO_MINAS", new string[] {"10", "9", "8", "7", "6", "5", "4", "3", "2", "1"}
                 }
             };
 
@@ -82,7 +107,7 @@ namespace LTK
             };
         }
 
-        readonly static string[] strJogos=  {
+        readonly static string[] strJogos =  {
             "QUINA", "MEGASENA", "LOTOFACIL", "LOTOMANIA", "DUPLASENA",
             "INTRALOT_MINAS_5", "INTRALOT_LOTO_MINAS", "INTRALOT_KENO_MINAS"};
 
@@ -93,16 +118,20 @@ namespace LTK
 
         private void frmGerador_Aleatorio_Load(object sender, EventArgs e)
         {
-            cmbJogo_Tipo.Items.AddRange(strJogos.ToArray());
+            // Preenche com o nome dos jogos.
+            for (var uA = 0; uA < jogoInfo.Length; uA++)
+                cmbJogo_Tipo.Items.Add(jogoInfo[uA].jogoTipo);
             cmbJogo_Tipo.SelectedIndex = 0;
 
-            cmbAposta_com.Items.AddRange(jogo_aposta["QUINA"].ToArray());
+            // Preenche a caixa de combinação 'cmbAposta_com' relativo ao primeiro jogo.
+            for (var uA = 0; uA < jogoInfo[0].bolasApostadas.Length;uA++)
+                cmbAposta_com.Items.Add(jogoInfo[0].bolasApostadas[uA]);
             cmbAposta_com.SelectedIndex = 0;
 
             // Vamos preencher o controle 'cmbAposta_Quantidade'.
-            for(var iA = 1; iA <= 1000; iA++)
+            for (var iA = 1; iA <= 1000; iA++)
             {
-                cmbAposta_Quantidade.Items.Add(iA + " jogo(s).");
+                cmbAposta_Quantidade.Items.Add(iA);
             }
 
             cmbAposta_Quantidade.SelectedIndex = 0;
@@ -115,21 +144,21 @@ namespace LTK
         /// <param name="e"></param>
         private void cmbJogo_Tipo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // Quando o usuário seleciona um outro jogo, devemos atualizar o controle 'cmbJogo_com' pois,
-            // cada jogo tem uma quantidade de números que devemos jogar.
-            string strJogo_Selecionado = cmbJogo_Tipo.Text.ToUpper();
-
-            // Vamos verificar se o jogo selecionado existe, isto geralmente, não ocorre mas por precaução.
-            if (!jogo_aposta.Keys.Contains(strJogo_Selecionado))
+            // O índice é baseado em zero.
+            // Se não há nada selecionado, -1 é retornado.
+            // O que geralmente, nunca ocorrerá pois este evento indica isso.
+            int indiceSelecionado = cmbJogo_Tipo.SelectedIndex;
+            
+            // Só altera se índice esta dentro do intervalo, geralmente, sempre irá funcionar.
+            if(indiceSelecionado > 0 && indiceSelecionado < jogoInfo.Length)
             {
-                MessageBox.Show("ERRO", "Jogo" + strJogo_Selecionado + " inexistente.", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+                cmbAposta_com.Items.Clear();
 
-            // Vamos atualizar o controle.
-            cmbAposta_com.Items.Clear();
-            cmbAposta_com.Items.AddRange(jogo_aposta[strJogo_Selecionado].ToArray());
-            cmbAposta_com.SelectedIndex = 0;
+                for (var uA = 0; uA < jogoInfo[indiceSelecionado].bolasApostadas.Length; uA++)
+                    cmbAposta_com.Items.Add(jogoInfo[indiceSelecionado].bolasApostadas[uA]);
+                cmbAposta_com.SelectedIndex = 0;
+                cmbAposta_Quantidade.SelectedIndex = 0;
+            }            
         }
 
         private void btnGerar_Click(object sender, EventArgs e)
@@ -144,7 +173,7 @@ namespace LTK
 
             strJogo_Aposta_com = strJogo_Aposta_com.Replace("com", "").Replace("número", "");
             strJogo_Aposta_com = strJogo_Aposta_com.Replace(".", "").Replace("(s)", "").Replace("s", "");
-            
+
             string strJogo_Quantidade = cmbAposta_Quantidade.Text.Replace("(s)", "").Replace("jogo", "").Replace(".", "").Trim();
 
             // Vamos verificar se o jogo existe.
@@ -183,7 +212,7 @@ namespace LTK
             for (var i = jogo_limite_inferior; i <= jogo_limite_superior; i++)
                 listaNumeros.Add(i);
 
-            
+
             // Vamos criar um arranjo do tipo bool, para guardar o status de cada bola
             // Devemos considerar a quantidade de ítens maior que 1 pois, os arranjos C#
             // são baseados em 0, e se a menor bola do jogo é 1, não podemos definir o arranjo
@@ -206,12 +235,12 @@ namespace LTK
             int qt_de_numeros_aleatorios = 0;
             int[,] jogo_aleatorio = new int[iJogo_Quantidade, iJogo_Aposta_com];
 
-            
+
             for (var iA = 0; iA < iJogo_Quantidade; iA++)
             {
                 int iB = 0;
-                
-                while(iB < iJogo_Aposta_com)
+
+                while (iB < iJogo_Aposta_com)
                 {
                     // A variável 'listaNumeros' guarda todos os números que ainda não foi selecionado.
                     int indiceNumero = gerador_aleatorio.Next(listaNumeros.Count);
@@ -243,7 +272,7 @@ namespace LTK
                         // Se a quantidade de números que compõe o jogo dividido pela quantidade de números apostados
                         // é diferente de zero, então devemos pegar todos os números da linha atual e definir
                         // como true no arranjo.
-                        if(qt_de_numeros_aleatorios == jogo_quantidade_de_itens)
+                        if (qt_de_numeros_aleatorios == jogo_quantidade_de_itens)
                         {
                             qt_de_numeros_aleatorios = 0;
 
@@ -260,7 +289,7 @@ namespace LTK
                             if (iB < iJogo_Aposta_com)
                             {
                                 qt_de_numeros_aleatorios = 0;
-                                for(var iC = 0; iC <= iB; iC++)
+                                for (var iC = 0; iC <= iB; iC++)
                                 {
                                     bNumero_ja_sorteado[jogo_aleatorio[iA, iC]] = true;
                                     qt_de_numeros_aleatorios++;
@@ -276,14 +305,14 @@ namespace LTK
                 }
 
                 // Ordenar números.
-                for(var uA = 0; uA < iJogo_Aposta_com; uA++)
+                for (var uA = 0; uA < iJogo_Aposta_com; uA++)
                 {
                     int uNumero_Anterior = jogo_aleatorio[iA, uA];
                     int uNumero_Proximo;
-                    for(var uB = uA + 1; uB < iJogo_Aposta_com; uB++)
+                    for (var uB = uA + 1; uB < iJogo_Aposta_com; uB++)
                     {
                         uNumero_Proximo = jogo_aleatorio[iA, uB];
-                        if(uNumero_Proximo< uNumero_Anterior)
+                        if (uNumero_Proximo < uNumero_Anterior)
                         {
                             jogo_aleatorio[iA, uA] = uNumero_Proximo;
                             jogo_aleatorio[iA, uB] = uNumero_Anterior;
@@ -299,17 +328,17 @@ namespace LTK
             //List<int> linhas_repetidas = new List<int>();
             Dictionary<int, List<int>> linha_repetida = new Dictionary<int, List<int>>();
 
-            for(var uA = 0; uA < iJogo_Quantidade; uA++)
+            for (var uA = 0; uA < iJogo_Quantidade; uA++)
             {
-                for(var uB = uA + 1; uB < iJogo_Quantidade; uB++)
+                for (var uB = uA + 1; uB < iJogo_Quantidade; uB++)
                 {
                     int numeros_repetidos = 0;
-                    for(var uC = 0; uC < iJogo_Aposta_com; uC++)
+                    for (var uC = 0; uC < iJogo_Aposta_com; uC++)
                     {
                         if (jogo_aleatorio[uA, uC] == jogo_aleatorio[uB, uC])
                             numeros_repetidos++;
                         else
-                            break;                        
+                            break;
                     }
                     // Achou número repetido guarda na lista.
                     if (numeros_repetidos == iJogo_Aposta_com)
@@ -334,7 +363,7 @@ namespace LTK
             // 2 colunas adicional:
             // 1 para o nome do jogo.
             // 1 para observação.
-            grJogos.ColumnCount = iJogo_Aposta_com+3;
+            grJogos.ColumnCount = iJogo_Aposta_com + 3;
 
             grJogos.RowCount = iJogo_Quantidade;
 
@@ -344,14 +373,14 @@ namespace LTK
             grJogos.Columns[2].Name = "OBSERVACAO";
 
             // Vamos colocar o nome das colunas.
-            for(var iA = 0; iA < iJogo_Aposta_com; iA++)
+            for (var iA = 0; iA < iJogo_Aposta_com; iA++)
             {
                 grJogos.Columns[iA + 3].Name = "B" + (iA + 1).ToString();
             }
 
 
-            
-            for(var iA = 0; iA < iJogo_Quantidade; iA++)
+
+            for (var iA = 0; iA < iJogo_Quantidade; iA++)
             {
                 grJogos.Rows[iA].Cells[0].Value = strJogo;
                 grJogos.Rows[iA].HeaderCell.Value = iA;
@@ -362,15 +391,15 @@ namespace LTK
                 if (linha_repetida.ContainsKey(iA))
                 {
                     string strTexto = "";
-                    foreach(var repetidos in linha_repetida[iA])
+                    foreach (var repetidos in linha_repetida[iA])
                     {
                         strTexto += repetidos.ToString() + ", ";
                     }
                     grJogos.Rows[iA].Cells[1].Value = "<REPETIU=LINHA-" + strTexto + ">";
                 }
-               
 
-                for(var iB = 0; iB < iJogo_Aposta_com; iB++)
+
+                for (var iB = 0; iB < iJogo_Aposta_com; iB++)
                 {
                     grJogos.Rows[iA].Cells[iB + 3].Value = jogo_aleatorio[iA, iB];
                 }
